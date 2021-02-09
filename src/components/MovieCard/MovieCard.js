7f2d0ebd48cbf63f5151fb8d2084d4111f9ebeed
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2021-02-09 17:15:43 +0200

    fix

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,7 +5,7 @@ import styles from './MovieCard.module.css';
 function MovieCard({ movie}) {
     return (
     <>
-        <img className={styles.img} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}?   api_key=892c9b9f1c704261a0f515abd746d990`} alt={movie.title}/>
+        <img className={styles.img} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}?api_key=892c9b9f1c704261a0f515abd746d990`} alt={movie.title}/>
                 <div className={styles.details}>
                     <h1 className={styles.title}>{movie.title} ({movie.release_date.slice(0, 4)})</h1>
                     <div className={styles.score}>User Score: {movie.vote_average * 10}%</div>
@@ -17,14 +17,17 @@ function MovieCard({ movie}) {
 }
 
 MovieCard.defaultProps = {
-  title: '',
-  id: 0,
+  img: '',
 };
-
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-            title: PropTypes.string,
-            id: PropTypes.number,})
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    img: PropTypes.string,
+    vote_average: PropTypes.number.isRequired,
+    overview: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    }),
 };
 
 export default MovieCard;
--- src/views/Cast/Cast.js
+++ src/views/Cast/Cast.js
@@ -2,33 +2,33 @@ import React, { Component } from 'react';
 import styles from './Cast.module.css';
 import API from "../../services/movies-api";
 import Load from "../../components/Loader/Loader";
+import CastItem from "../../components/CastItem/CastItem";
 
 export default class Cast extends Component {
    state = {
        credits: '',
        loading: false,
+       error: null,
     };
 
     async componentDidMount() {
+        
         try {
             this.setState({ loading: true });
             const movieId = this.props.match.params.movieId;
             const response = await API.showCast(movieId);
             this.setState({ credits: response.data.cast });
             }
-        catch (error) {
-            this.setState({ error: error })
-        }
+        catch (error){ this.setState({ error: error }) }
         finally { this.setState({ loading: false }); }
     };
 
     render() {
         const { credits, error } = this.state;
-
         return (
             <>
                 <hr/>
-                {error && <h1>Error, try again later</h1>}
+                {error && <h1>{error.message}</h1>}
                 {this.state.loading &&
                     <Load
                         type="ThreeDots"
@@ -40,13 +40,7 @@ export default class Cast extends Component {
                 {credits.length > 0 && (
                     <ul className={styles.cast}>
                         {credits.map(credit => (
-                            <li className={styles.item} key={credit.id}>
-                                
-                                <img className={styles.img} src={`https://image.tmdb.org/t/p/w500/${credit.profile_path}?api_key=892c9b9f1c704261a0f515abd746d990`} alt={credit.name} />
-                                <div className={styles.name}>{credit.name}</div>
-                                <div className={styles.character}>Character: {credit.character}</div>
-                                <hr/>
-                            </li>
+                            <CastItem credit={credit} key={credit.id}></CastItem>
                         ))}
                     </ul>
                 )}
